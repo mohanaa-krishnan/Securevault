20e61b176ed761940348ce5a4708053e59e2d3c0
Mohanaa Krishnan <mohanaakrishnan12@gmail.com>
2026-07-01 11:47:48 +0530

    feat: implement SecureVault CLI with login and vault CRUD operations

--- backend/src/Main.java
+++ backend/src/Main.java
@@ -37,11 +37,136 @@ public class Main {
 if(found)
 {
     System.out.println("Login successful");
+
+    ArrayList<Vaultentry>vaults=new ArrayList<>();
+    boolean running=true;
+    while(running) {
+    System.out.println("0.nothing");
+    System.out.println("1.add credentials");
+    System.out.println("2.view credentials");
+    System.out.println("3.Delete credentials");
+    System.out.println("4.edit credentials");
+
+    int check=in.nextInt();
+    in.nextLine();
+
+
+        if (check == 1) {
+            System.out.println("enter website or app name:");
+            String website = in.nextLine();
+            System.out.println("enter username/email");
+            String email = in.nextLine();
+            System.out.println("enter password");
+            String password = in.nextLine();
+            Vaultentry v = new Vaultentry(website, email, password);
+            vaults.add(v);
+        }
+       else  if (check == 2) {
+            if (vaults.isEmpty()) {
+                System.out.println("Vault is empty");
+            } else {
+                for (Vaultentry entry : vaults) {
+                    System.out.println("website:" + entry.website);
+                    System.out.println("email/username:" + entry.username);
+                    System.out.println("password:" + entry.password);
+                }
+            }
+        }
+       else  if(check==3)
+        {
+            if(vaults.isEmpty())
+            {
+                System.out.println("Vault is empty");
+            }
+            else
+            {
+                System.out.println("Websites");
+                for(Vaultentry entry :vaults)
+                {
+                    System.out.println(entry.website);
+                }
+                System.out.println("Choose the website:");
+                String temp=in.nextLine();
+                boolean found2=false;
+                for(int i=0;i<vaults.size();i++)
+                {
+                    if(temp.equals(vaults.get(i).website))
+                    {
+                        found2=true;
+                        vaults.remove(i);
+                    }
+                }
+                if(found2)
+                {
+                    System.out.println("Removed successfully");
+                }
+                else
+                {
+                    System.out.println("Not such credentials exsits");
+                }
+            }
+        }
+       else if(check==4)
+        {
+            if(vaults.isEmpty())
+            {
+                System.out.println("Vault is empty");
+            }
+            else
+            {
+                System.out.println("Websites");
+                for(Vaultentry entry:vaults)
+                {
+                    System.out.println(entry.website);
+                }
+                System.out.println("Choose the website:");
+                String temp=in.nextLine();
+                boolean found2=false;
+                for(Vaultentry entry :vaults)
+                {
+                    if(temp.equals(entry.website))
+                    {
+                        found2=true;
+                        System.out.println("1.password update");
+                        System.out.println("2.email update");
+                        int check2=in.nextInt();
+                        in.nextLine();
+                        if(check2==1)
+                        {
+                            System.out.println("Enter new password:");
+                            String newpassword=in.nextLine();
+                            entry.password=newpassword;
+                        } else if (check2==2) {
+                            System.out.println("Enter new mail:");
+                            String newemail=in.nextLine();
+                            entry.username=newemail;
+
+                        }
+
+                    }
+                }
+                if(found2)
+                {
+                    System.out.println("Updated successfully");
+                }
+                else
+                {
+                    System.out.println("Not such credentials exsits");
+                }
+            }
+        }
+       else if(check==0)
+        {
+            running=false;
+            System.out.println("Out of process");
+        }
+    }
 }
 else
 {
     System.out.println("Login failed");
 }
+
 
     }
 }
---
+++ backend/src/Vaultentry.java
@@ -0,0 +1,12 @@
+public class Vaultentry {
+    String website;
+    String username;
+    String password;
+
+    Vaultentry(String website,String username,String password)
+    {
+        this.website=website;
+        this.username=username;
+        this.password=password;
+    }
+}
